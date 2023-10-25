820c0c09b94a2fd0d9480d87a0d616ce433670f4
joosun0823 <sun10220466@gmail.com>
2023-10-25 15:40:52 +0900

    23.10.25

---
+++ app/page.js
@@ -0,0 +1,13 @@
+"use client";
+import "./globals.css";
+import pageSt from "./page.module.scss";
+import Login from "./comp/Login";
+import { useEffect } from "react";
+
+export default function Home() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
